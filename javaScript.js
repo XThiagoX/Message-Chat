8f90d12a677d52e0949e72ae8fc0b4dc8ec7a356
Thiago Araujo <thiago_a@id.uff.br>
2022-07-29 18:32:34 -0300

    feat/fix/style(visualização de mensagens):corrigida a visualização dos elemntos de mensagem

--- javaScript.js
+++ javaScript.js
@@ -4,6 +4,9 @@ let sendMessage = document.querySelector('.send')
 let messageBlock = document.querySelector('.massage-block')
 let tagP = document.querySelector('.message')
 let text = document.querySelector('#textarea')
+
+let messageField = document.querySelector('.messages')
+
 let messagesSends = 0
 
 let body = document.querySelector('body')
@@ -25,32 +28,36 @@ sendMessage.addEventListener('click', () => {
     divMassageblock.setAttribute('class','massage-block')
 
     let divMessage = document.createElement('div')
-    divMessage.setAttribute('class','massage')
+    divMessage.setAttribute('class','message')
 
     let divMessageButtons = document.createElement('div')
     divMessageButtons.setAttribute('class','message-buttons')
 
     let buttonMessageEdit = document.createElement('div')
     buttonMessageEdit.setAttribute('class','buttonMessageEdit')
+    buttonMessageEdit.textContent = "Editar"
     
     let buttonMessageDelete = document.createElement('div')
     buttonMessageDelete.setAttribute('class','buttonMessageDelete')
+    buttonMessageDelete.textContent = "Editar"
 
     // ATRIBUÇÃO PARA MONTAGEM DE DO BLOCO
     let paragraph = document.createElement('p')
+    let tagP = document.querySelector('.message')
     paragraph.innerHTML = text.value   
-    tagP.append(paragraph)  
-    divMessage.append(tagP)
+    //tagP.append(paragraph)  
+    divMessage.append(paragraph)
     divMassageblock.append(divMessage)
     divMessageButtons.append(buttonMessageEdit)
     divMessageButtons.append(buttonMessageDelete)
     divMassageblock.append(divMessageButtons)
+    messageField.append(divMassageblock)
 
     messageBlock.removeAttribute('hidden')    
     // let paragraph = document.createElement('p')
     // paragraph.innerHTML = text.value   
     // tagP.append(paragraph) 
-    console.log(paragraphs.textContent)
+    console.log(paragraph.textContent)
     // tagP.textContent = paragraph.value   
     text.value = ""
     messagesSends++
