873ff8288f69a0863e9b8f796d729920353326d9
Thiago Araujo <thiago_a@id.uff.br>
2022-07-30 18:15:38 -0300

    feat(Edição de mensagens): adicionada novo método da para chamar a função de editção do botão, funcionado corretamente

--- javaScript.js
+++ javaScript.js
@@ -2,85 +2,116 @@
 let deleteMessage = document.querySelector('.buttonMessageDelete')
 let editMessage = document.querySelector('.buttonMessageEdit')
 let sendMessage = document.querySelector('.send')
-// DIV'S DE CONTEÚDO
+
+// DIV'S DE CONTEÚDO CRIADAS DINAMICAMENTE
 let messageBlock = document.querySelector('.massage-block')
 let messageField = document.querySelector('.messages')
+let divMassageblock = document.createElement('div')
+//let divMessage = document.createElement('div')
 let tagP = document.querySelector('.message')
+
+// BOTÕES CRIADOS DINAMICAMENTE
+//let buttonMessageEdit = document.createElement('button')
 
 // ENTRADA
 let text = document.querySelector('#textarea')
+
 // VARIÁVEIS AUXILIARES
+let paragraphEdited = document.createElement('p')
+// let paragraph = document.createElement('p')
 let messagesSends = 0
+let buttonPressed = false
+let messageBeingEdited = false
 
-// buttonMessageEdit.setAttribute('onclick', EditMessage(this))
-// buttonMessageDelete.setAttribute('onclick', DeleteMessage(this))
-function DeleteMessage(){
-    messageBlock.remove()
-}
-function EditMessage(){
-    document.getElementsByTagName("textarea")[0].style.border = "solid 2px #F6A75D"
-    text.value = tagP.textContent
-    tagP.innerHTML = ""
+// FUNÇÕES
+function DeleteMessage(){    
+    divMassageblock.remove()
 }
 
-editMessage.addEventListener('click', () => {        
-    document.getElementsByTagName("textarea")[0].style.border = "solid 2px #F6A75D"
-    text.value = tagP.textContent
-    tagP.innerHTML = ""
+function EditMessage(paragraph, buttonMessageEdit, divMessage){       
+    if(buttonPressed == true){                 
+        document.getElementsByTagName("textarea")[0].style.border = "solid 0.5px #000"            
+        buttonMessageEdit.textContent = "Editar"                    
+        // let divMessage = document.createElement('div')
+        paragraph.innerText =  text.value 
+        console.log(paragraph) 
+        divMessage.append(paragraph)          
+        text.value = " " 
+        messageBeingEdited = false           
+        // buttonMessageEdit.setAttribute('onclick', 'EditMessage(this)')                
+    }
 
-})
+    if(buttonPressed == false){
+        document.getElementsByTagName("textarea")[0].style.border = "solid 2px #F6A75D"            
+        text.value = paragraph.textContent
+        paragraph.innerHTML = " "
+        buttonMessageEdit.textContent = "Enviar"
+        buttonPressed = true        
+        messageBeingEdited = true
+        // buttonMessageEdit.setAttribute('onclick', 'EditMessage(this)')
+    }            
+}
 
-deleteMessage.addEventListener('click', () => {         
-    messageBlock.remove()
-})
-
-sendMessage.addEventListener('click', () => {  
-    console.log("ok")
+sendMessage.addEventListener('click', () => {     
     if(text.value != ""){
-        // CRIAÇÃO DO BLOCO QUE DEFINE A MENSAGEM
-        let divMassageblock = document.createElement('div')
-        divMassageblock.setAttribute('class','massage-block')
+        if(messageBeingEdited == false){
+            // CRIAÇÃO DO BLOCO QUE DEFINE A MENSAGEM
+            let divMassageblock = document.createElement('div')
+            divMassageblock.setAttribute('class','massage-block')
 
-        let divMessage = document.createElement('div')
-        divMessage.setAttribute('class','message')
+            let divMessage = document.createElement('div')
+            divMessage.setAttribute('class','message')
 
-        let divMessageButtons = document.createElement('div')
-        divMessageButtons.setAttribute('class','message-buttons')
+            let divMessageButtons = document.createElement('div')
+            divMessageButtons.setAttribute('class','message-buttons')
 
-        let buttonMessageEdit = document.createElement('button')
-        buttonMessageEdit.setAttribute('class','buttonMessageEdit')
-        buttonMessageEdit.textContent = "Editar"        
-        
-        let buttonMessageDelete = document.createElement('button')
-        buttonMessageDelete.setAttribute('class','buttonMessageDelete')
-        buttonMessageDelete.textContent = "Excluir"    
+            let buttonMessageEdit = document.createElement('button')
+            buttonMessageEdit.setAttribute('class','buttonMessageEdit')
+            buttonMessageEdit.textContent = "Editar"      
+            //buttonMessageEdit.setAttribute('onclick', "EditMessage(this)")
+            
+            let buttonMessageDelete = document.createElement('button')
+            buttonMessageDelete.setAttribute('class','buttonMessageDelete')
+            buttonMessageDelete.textContent = "Excluir"    
+            buttonMessageDelete.setAttribute('onclick', "DeleteMessage(this)")
+    
+            // ATRIBUÇÃO PARA MONTAGEM DE DO BLOCO 
+            let paragraph = document.createElement('p')
+            paragraph.innerHTML = text.value   
+            paragraphEdited = paragraph
+            
+            // console.log(paragraphEdited.innerHTML)
+            // console.log(paragraphEdited.textContent)
+            // console.log(paragraph.textContent)
+            // console.log(paragraph.innerHTML)
 
-        // ATRIBUÇÃO PARA MONTAGEM DE DO BLOCO
-        let paragraph = document.createElement('p')
-        let tagP = document.querySelector('.message')
-        paragraph.innerHTML = text.value   
-        //tagP.append(paragraph)  
-        divMessage.append(paragraph)
-        divMassageblock.append(divMessage)
-        divMessageButtons.append(buttonMessageEdit)
-        divMessageButtons.append(buttonMessageDelete)
-        divMassageblock.append(divMessageButtons)
-        messageField.append(divMassageblock)    
+            divMessage.append(paragraph)
+            divMassageblock.append(divMessage)
+            divMessageButtons.append(buttonMessageEdit)
+            divMessageButtons.append(buttonMessageDelete)
+            divMassageblock.append(divMessageButtons)
+            messageField.append(divMassageblock)    
+                
+            // let paragraph = document.createElement('p')
+            // paragraph.innerHTML = text.value   
+            // tagP.append(paragraph)            
+            // tagP.textContent = paragraph.value   
+            text.value = ""
+            messagesSends++
+            
+            buttonMessageEdit.addEventListener('click', ()=> { 
+                EditMessage(paragraph,buttonMessageEdit,divMessage) 
+            })
 
-        //messageBlock.removeAttribute('hidden')    
-        // let paragraph = document.createElement('p')
-        // paragraph.innerHTML = text.value   
-        // tagP.append(paragraph) 
-        console.log(paragraph.textContent)
-        // tagP.textContent = paragraph.value   
-        text.value = ""
-        messagesSends++
-
-        buttonMessageEdit.setAttribute('onclick', EditMessage(this))
-        buttonMessageDelete.setAttribute('onclick', DeleteMessage(this))
-        
+            buttonMessageDelete.addEventListener('click', ()=> { 
+                DeleteMessage() 
+            })
+        }
+        else{
+            alert("Para enviar a mensagem que deseja editar aperte o mesmo botão que agora foi renomeado para Enviar")
+        }     
     }
     else{
-        alert("Digite algo para poder enviar")
-    }
+            alert("Digite algo para poder enviar")
+        }    
 })
